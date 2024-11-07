4081f8c017761c66f01223685bedffaae48b817b
FREDYK1 <fkwekukankam@gmail.com>
2024-11-07 15:24:34 +0000

    Retrieve the data form the form

--- app.py
+++ app.py
@@ -1,9 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
-@app.route('/')
+@app.route('/', methods=["GET", "POST"])
 def index():
+    if request.method == "POST":
+        first_name = request.form["first_name"]
+        last_name = request.form["last_name"]
+        email = request.form["email"]
+        date = request.form["date"]
+        occupation = request.form["occupation"]
+        print(f"First Name: {first_name} \nLast Name: {last_name} \nEmail: {email} \nDate: {date} \nOccupation: {occupation}")
+
     return render_template('index.html')
 
-app.run(debug=True, port = 5001)
+
+app.run(debug=True, port=5001)
